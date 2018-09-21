70574eb8cdc9bca213add35ac93194cb39eb2b69
roborhin0 <38258826+roborhin0@users.noreply.github.com>
2018-09-20 23:08:18 -0700

    Added TODO list

--- main.cpp
+++ main.cpp
@@ -7,11 +7,25 @@
 */
 
 // TODO
-/**  Plan for parsing data strings:
-*    - Regex tag match
-*    - Regex name match
-*    - Compare name to attributes
-*    - Regex dot match or value match depending on the name
+/**  1. Implement parsing data strings functions:
+*    	- Regex tag match
+*    	- Regex name match
+*    	- Compare name to attributes
+*    	- Regex dot match or value match depending on the name
+*
+*    2. Create input function that uses the Game::m_Commands list in TxtGameEngine/TxtGameEngine.h (also change the valid template function from TxtGameEngine/Utils.h to get and entire line instead of one word at a time)
+*
+*    3. Crap ton of Getters and Setters needed in all classes
+*
+*    4. Make more functions to load data from files for Quest, Item, Spell, and Entity classes in TxtGameEngine/TxtGameEngine.h
+*
+*    5. Create Status class that passively affects the Character
+*
+*    6. LoadSaveData function needed
+*
+*    7. Encounters needed
+*
+*    8. Character::levelUp() function needed
 */
 
 // Regular Expressions
