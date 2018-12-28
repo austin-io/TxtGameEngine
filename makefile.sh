583f1fa1b5c95851d27a9c2aa3771cc5c9939e87
AZ <apoberezhnik23@gmail.com>
2018-12-28 04:16:00 +0000

    Fixed makefile

--- makefile.sh
+++ makefile.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-g++ main.cpp TxtGameEngine/*.cpp -I ~/TxtGameEngine/ -o out 
-=======
-g++ main.cpp TxtGameEngine/*.cpp -I ./TxtGameEngine/ -o out -time 
->>>>>>> a58fc7552e197b056e931f64fdb1c17856683e7e
+g++ main.cpp TxtGameEngine/*.cpp -I ./TxtGameEngine/ -o out 
